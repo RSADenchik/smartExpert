14a1fbdcd5a979ab6a44161b4cf1d090d173e04d
RSADenchik <WorldRSA@gmail.com>
2022-03-24 02:58:28 +0200

    add git

--- src/router/index.js
+++ src/router/index.js
@@ -21,9 +21,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory('/smartExpert/'),
-  routes: [
-      '/select', '/tooltip'
-  ]
+  routes
 })
 
 export default router
